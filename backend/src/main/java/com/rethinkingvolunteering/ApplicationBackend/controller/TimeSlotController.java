91c890606b5c1422c0fa2b8b9f9ba90025389276
Olivia Meyer <olivia-meyer-02@outlook.de>
2026-01-13 13:23:06 +0100

    Timeslots filtered by Topic

--- backend/src/main/java/com/rethinkingvolunteering/ApplicationBackend/controller/TimeSlotController.java
+++ backend/src/main/java/com/rethinkingvolunteering/ApplicationBackend/controller/TimeSlotController.java
@@ -5,6 +5,7 @@ package com.rethinkingvolunteering.ApplicationBackend.controller;
 import com.rethinkingvolunteering.ApplicationBackend.entity.TimeSlot;
 import com.rethinkingvolunteering.ApplicationBackend.entity.Volunteer;
 import com.rethinkingvolunteering.ApplicationBackend.enums.Location;
+import com.rethinkingvolunteering.ApplicationBackend.enums.Topic;
 import com.rethinkingvolunteering.ApplicationBackend.repository.TimeSlotRepository;
 import com.rethinkingvolunteering.ApplicationBackend.service.TimeSlotService;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -18,45 +19,28 @@ import java.util.List;
 @RequestMapping("/api/timeslots")
 public class TimeSlotController {
 
-    private final TimeSlotRepository timeSlotRepository;
     private final TimeSlotService timeSlotService;
 
-    public TimeSlotController(TimeSlotRepository timeSlotRepository, TimeSlotService timeSlotService) {
-        this.timeSlotRepository = timeSlotRepository;
+    public TimeSlotController(TimeSlotService timeSlotService) {
         this.timeSlotService = timeSlotService;
     }
 
-    /*
-
-    @PostMapping("/addTimeSlot")
-    public void addTimeSlot(@RequestBody AddAppointmentRequest body) {
-        timeSlotService.addAppointment(
-                body.email(),
-                body.location(),
-                body.date(),
-                body.time(),
-                body.topic());
-    }
-
-     */
-
     @GetMapping("/locations")
-    public List<Location> getAvailableLocations() {
-        return this.timeSlotRepository.findAllLocations();
+    public List<Location> getAvailableLocations(@RequestParam GetLocationsRequest getLocationsRequest) {
+        return timeSlotService.getAvailableLocations(getLocationsRequest.topic);
     }
 
     @GetMapping("/dates")
-    public List<LocalDate> getAvailableDates(@RequestParam Location location) {
-        return this.timeSlotService.getAvailableDates(location);
+    public List<LocalDate> getAvailableDates(@RequestParam GetDatesRequest req) {
+        return this.timeSlotService.getAvailableDates(req.topic, req.location);
     }
 
     @GetMapping("/timeslots")
-    public List<LocalDateTime> getAvailableTimeSlots(
-            @RequestParam Location location,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-        return this.timeSlotService.getAvailableTimeSlots(location, date);
+    public List<LocalDateTime> getAvailableTimeSlots(@RequestParam GetTimeslotsRequest req) {
+        return this.timeSlotService.getAvailableTimeSlots(req.topic, req.location, req.date);
     }
 
-    public record AddAppointmentRequest(TimeSlot timeslot, Volunteer volunteer) {}
-
+    public record GetLocationsRequest(Topic topic) {}
+    public record GetDatesRequest(Topic topic, Location location) {}
+    public record GetTimeslotsRequest(Topic topic, Location location, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {}
 }
--- backend/src/main/java/com/rethinkingvolunteering/ApplicationBackend/service/TimeSlotService.java
+++ backend/src/main/java/com/rethinkingvolunteering/ApplicationBackend/service/TimeSlotService.java
@@ -3,6 +3,7 @@ package com.rethinkingvolunteering.ApplicationBackend.service;
 import com.rethinkingvolunteering.ApplicationBackend.entity.TimeSlot;
 import com.rethinkingvolunteering.ApplicationBackend.entity.Volunteer;
 import com.rethinkingvolunteering.ApplicationBackend.enums.Location;
+import com.rethinkingvolunteering.ApplicationBackend.enums.Topic;
 import com.rethinkingvolunteering.ApplicationBackend.repository.TimeSlotRepository;
 import com.rethinkingvolunteering.ApplicationBackend.repository.VolunteerRepository;
 import org.springframework.stereotype.Service;
@@ -24,14 +25,21 @@ public class TimeSlotService {
         this.volunteerRepository = volunteerRepository;
     }
 
-    public List<LocalDate> getAvailableDates(Location location) {
-        return timeSlotRepository.findByLocationEquals(location).stream()
+    public List<Location> getAvailableLocations(Topic topic){
+        return timeSlotRepository.findByTopic(topic).stream()
+                .filter(o->!o.isBooked())
+                .map(TimeSlot::getLocation)
+                .distinct().toList();
+    }
+
+    public List<LocalDate> getAvailableDates(Topic topic, Location location) {
+        return timeSlotRepository.findByTopicAndLocation(topic, location).stream()
                 .map(o -> o.getStartTime().toLocalDate())
                 .sorted().collect(Collectors.toList());
     }
 
-    public List<LocalDateTime> getAvailableTimeSlots(Location location, LocalDate date) {
-        return timeSlotRepository.findByLocationEquals(location).stream()
+    public List<LocalDateTime> getAvailableTimeSlots(Topic topic, Location location, LocalDate date) {
+        return timeSlotRepository.findByTopicAndLocation(topic, location).stream()
                 .map(TimeSlot::getStartTime)
                 .filter(startTime -> startTime.toLocalDate().equals(date))
                 .sorted()
